5778411c236f193c99cfab103ede480d3670f52d
iamsiraj30 <sirajul.islam1@softvence.com>
2026-06-19 08:19:32 +0600

    push

--- src/middleware/authGurd.ts
+++ src/middleware/authGurd.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { UserRole } from "../generated/prisma/enums";
+import { UserRole } from "@prisma/client";
 
 type DecodedToken = {
   userId: string;
